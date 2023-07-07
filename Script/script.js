53c2b912f79a1e155fda24bb470573d3cdeda222
Mz-Shed <shedrachmkpuma1@gmail.com>
2023-07-07 13:50:45 +0100

    This is my commitment

---
+++ Script/script.js
@@ -0,0 +1,16 @@
+
+
+let firstName = "Shedrach"
+let lastName = "mkpuma"
+let quote = "A thousand miles begins with a single step"
+
+console.log(firstName)
+
+console.log(quote)
+
+let Principal = 100000;
+let Rate = 20;
+let Time = 2;
+let Interest = (Principal * Rate * Time) /100;
+
+console.log(Interest);
