5c7c3ebd6c2b1985c67df0d90606244bd42238d8
Mz-Shed <shedrachmkpuma1@gmail.com>
2023-07-08 14:34:26 +0100

    This is my commitment

--- Script/script.js
+++ Script/script.js
@@ -3,9 +3,10 @@
 let firstName = "Shedrach"
 let lastName = "mkpuma"
 let quote = "A thousand miles begins with a single step"
+let uppercase = firstName.toUpperCase()
+console.log(uppercase)
 
-console.log(firstName)
-
+console.log(uppercase.toLowerCase() )
 console.log(quote)
 
 let Principal = 100000;
@@ -14,3 +15,15 @@ let Time = 2;
 let Interest = (Principal * Rate * Time) /100;
 
 console.log(Interest);
+
+// Hour
+// If hour is between 6am and 12pm: Good morning
+// If its between 12pm and 6pm:Good evening
+
+let hour = 24;
+if (hour >= 6 && hour < 12)
+  console.log('Good morning');
+else if(hour >= 12 && hour < 18)
+  console.log('Good afternoon');
+else
+  console.log('Good evening');  
