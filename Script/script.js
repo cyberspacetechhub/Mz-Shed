4b46567428f33a65fb142f952c409da7ea9daf57
Mz-Shed <shedrachmkpuma1@gmail.com>
2023-07-10 14:25:24 +0100

    This is my commitment

--- Script/script.js
+++ Script/script.js
@@ -1,12 +1,6 @@
 
-
-let firstName = "Shedrach"
-let lastName = "mkpuma"
 let quote = "A thousand miles begins with a single step"
-let uppercase = firstName.toUpperCase()
-console.log(uppercase)
 
-console.log(uppercase.toLowerCase() )
 console.log(quote)
 
 let Principal = 100000;
@@ -27,3 +21,48 @@ else if(hour >= 12 && hour < 18)
   console.log('Good afternoon');
 else
   console.log('Good evening');
+
+let greeting = ' leave me alone '
+console.log(greeting)
+console.log(greeting.trim());
+
+let tvShow = 'catdog' ;
+
+tvShow.indexOf('cat');
+console.log(tvShow.indexOf ('cat'))
+tvShow.indexOf('dog');
+console.log(tvShow.indexOf ('dog'))
+tvShow.indexOf('z');
+console.log(tvShow.indexOf ('z'))
+
+let str = ' \'The downfall of a man is not the \n end of his life \' \\ '
+console.log(str.slice(5,13))
+
+str = str.replace('downfall','fall')
+console.log(str)
+
+let firstName = "Shedrach"
+let lastName = "mkpuma"
+console.log(`The full name is ${firstName} ${lastName}`)
+
+let cat1 = 20
+let cat2 = 10
+let exam = 50
+let totalScore = cat1 + cat2 + exam;
+
+console.log(`The totalScore is ${totalScore}`)
+
+let news = ' The BURNING MAN is a festival in America '
+console.log(`This is for non trim ${(news)}`)
+console.log(`This is for trim ${news.trim()}`)
+
+console.log(`The index of man is ${news.indexOf ('MAN')}`)
+
+news = news.replace ('America','Europe')
+console.log(`This is for replace of America to Europe ${news}`)
+
+console.log(`This is for toUpperCase ${news.toUpperCase(news)}`)
+
+console.log(`This is for toLowerCase ${news.toLowerCase(news)}`)
+
+console.log(`This is for slice ${news.slice(22,31)}`)
